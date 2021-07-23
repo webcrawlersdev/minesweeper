dcc8d2e965dddb7e3d8a7d61dca7839ecb774625
Igor Bedesqui <igorbedesqui@gmail.com>
2021-07-22 21:17:40 -0300

    feat: added fisherYatesShuffle

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,21 @@
-export default function Home() {
-  return <div>empty</div>;
-}
+/**  Credit:
+ * http://bost.ocks.org/mike/shuffle/
+ */
+const fisherYatesShuffle = function (array) {
+  let m = array.length,
+    t,
+    i;
+
+  // While there remain elements to shuffle…
+  while (m) {
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+
+  return array;
+};
