d9b1eec13e26481a9c472749f709d4f5bd02a983
Igor Bedesqui <igorbedesqui@gmail.com>
2021-07-23 13:28:57 -0300

    refactor: removed debug useEffects

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,22 +32,6 @@ export default function Board() {
     setBoard(create_board(dimSize, bombNumber));
   };
 
-  useEffect(() => {
-    resetGame();
-  }, []);
-
-  useEffect(() => {
-    //console.table(board);
-  }, [board]);
-
-  useEffect(() => {
-    // console.log(won);
-  }, [won]);
-
-  useEffect(() => {
-    console.log(dugCellsNum);
-  }, [dugCellsNum]);
-
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="mb-6">Bombs: {bombNumber}</h1>
@@ -91,74 +75,75 @@ const Cell = memo(
     board;
     setBoard;
   }) => {
-  const [, setDugCellsNum] = useAtom(dugCellsNumAtom);
-  const [, setLost] = useAtom(lostAtom);
+    const [, setDugCellsNum] = useAtom(dugCellsNumAtom);
+    const [, setLost] = useAtom(lostAtom);
 
-  const dig = useCallback((row, col, board) => {
-    if (board[row][col].includes("h")) {
-      setDugCellsNum(dugCells.push(`${row}-${col}`));
-    }
+    const dig = useCallback((row, col, board) => {
+      if (board[row][col].includes("h")) {
+        setDugCellsNum(dugCells.push(`${row}-${col}`));
+      }
 
-    if (board[row][col].includes("*")) {
-      setLost(true);
-    }
+      if (board[row][col].includes("*")) {
+        setLost(true);
+      }
 
-    if (board[row][col].includes("0") && board[row][col].includes("h")) {
-      for (let r = -1; r <= 1; r++) {
-        for (let c = -1; c <= 1; c++) {
-          let checking_row = row + r;
-          let checking_col = col + c;
-          if (
-            // we don't need to check the cell we passed
-            !(checking_row == row && checking_col == col)
-          ) {
+      if (board[row][col].includes("0") && board[row][col].includes("h")) {
+        for (let r = -1; r <= 1; r++) {
+          for (let c = -1; c <= 1; c++) {
+            let checking_row = row + r;
+            let checking_col = col + c;
             if (
-              // checking_row and checking_col are inside the board
-              checking_row >= 0 &&
-              checking_row <= board.length - 1 &&
-              checking_col >= 0 &&
-              checking_col <= board.length - 1
+              // we don't need to check the cell we passed
+              !(checking_row == row && checking_col == col)
             ) {
               if (
-                !dugCells.find(
-                  (element) => element == `${checking_row}-${checking_col}`
-                )
+                // checking_row and checking_col are inside the board
+                checking_row >= 0 &&
+                checking_row <= board.length - 1 &&
+                checking_col >= 0 &&
+                checking_col <= board.length - 1
               ) {
-                dig(checking_row, checking_col, board);
+                if (
+                  !dugCells.find(
+                    (element) => element == `${checking_row}-${checking_col}`
+                  )
+                ) {
+                  dig(checking_row, checking_col, board);
+                }
               }
             }
           }
         }
       }
-    }
-    setBoard(
-      produce((draft) => {
-        draft[row][col] = draft[row][col].replace("h", "v");
-      })
-    );
-  }, []);
+      setBoard(
+        produce((draft) => {
+          draft[row][col] = draft[row][col].replace("h", "v");
+        })
+      );
+    }, []);
 
-  const value = item[0];
-  const flag = item[1];
-  const open = item[2];
+    const value = item[0];
+    const flag = item[1];
+    const open = item[2];
 
-  return (
-    <div
-      className={`w-5 md:w-10 focus-within:ring ring-yellow-300 rounded ${
-        open.includes("h") ? "bg-gray-300" : "bg-green-300"
-      }`}
-    >
-      <button
-        className="w-full h-full"
-        onClick={() => {
-          dig(row, col, board);
-        }}
+    return (
+      <div
+        className={`w-5 md:w-10 focus-within:ring ring-yellow-300 rounded ${
+          open.includes("h") ? "bg-gray-300" : "bg-green-300"
+        }`}
       >
-        {open == "v" ? value : flag != "_" ? flag : " "}
-      </button>
-    </div>
-  );
-});
+        <button
+          className="w-full h-full"
+          onClick={() => {
+            dig(row, col, board);
+          }}
+        >
+          {open == "v" ? value : flag != "_" ? flag : " "}
+        </button>
+      </div>
+    );
+  }
+);
 
 const create_board = (dim_size, bomb_number) => {
   // create an array with the amount of cells the board will have
