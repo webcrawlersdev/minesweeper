5f9c6f97b248aea1e9e9c533a7568d6b8a7cb2f6
Igor Bedesqui <igorbedesqui@gmail.com>
2021-07-22 21:18:31 -0300

    feat: added getNumOfNeighboringBombs

--- pages/index.jsx
+++ pages/index.jsx
@@ -30,3 +30,32 @@ const singleToMultiDimentionalArray = (initial_array, num_cols) => {
 
   return multi_dimentional_array;
 };
+
+const getNumOfNeighboringBombs = (board, row, col) => {
+  let num = 0;
+
+  for (let r = -1; r <= 1; r++) {
+    for (let c = -1; c <= 1; c++) {
+      let checking_row = row + r;
+      let checking_col = col + c;
+      if (
+        // we don't need to check the cell we passed
+        !(checking_row == row && checking_col == col)
+      ) {
+        if (
+          // checking_row and checking_col are inside the board
+          checking_row >= 0 &&
+          checking_row <= board.length - 1 &&
+          checking_col >= 0 &&
+          checking_col <= board.length - 1
+        ) {
+          if (board[checking_row][checking_col].includes("*")) {
+            num = num + 1;
+          }
+        }
+      }
+    }
+  }
+
+  return num;
+};
