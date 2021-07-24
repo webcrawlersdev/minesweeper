74968c850ac337a3364cf18acdd9b1a41cf1b958
Igor Bedesqui <igorbedesqui@gmail.com>
2021-07-24 15:56:43 -0300

    feat: abstracted Cell and store

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,14 @@
-import { memo, useEffect, useState, useCallback, useRef } from "react";
-import { atom, useAtom } from "jotai";
-import produce from "immer";
+import { useEffect, useState, useRef } from "react";
+import { useAtom } from "jotai";
 
-const dugCellsNumAtom = atom(0);
-const dimSizeAtom = atom(16);
-const bombNumberAtom = atom(40);
-const lostAtom = atom(false);
-
-const wonAtom = atom(
-  (get) =>
-    get(dugCellsNumAtom) ==
-      get(dimSizeAtom) * get(dimSizeAtom) - get(bombNumberAtom) &&
-    !get(lostAtom)
-);
+import Cell from "../components/Cell";
+import {
+  wonAtom,
+  lostAtom,
+  dugCellsNumAtom,
+  dimSizeAtom,
+  bombNumberAtom,
+} from "../lib/store";
 
 export default function Board() {
   const [won] = useAtom(wonAtom);
@@ -65,91 +61,5 @@ export default function Board() {
     </div>
   );
 }
-
-const Cell = memo(
-  ({
-    item,
-    row,
-    col,
-    board,
-    setBoard,
-    dugCellsRef,
-  }: {
-    item: string;
-    row: number;
-    col: number;
-    board;
-    setBoard;
-    dugCellsRef;
-  }) => {
-    const [, setDugCellsNum] = useAtom(dugCellsNumAtom);
-    const [, setLost] = useAtom(lostAtom);
-
-    const dig = useCallback((row, col, board) => {
-      if (board[row][col].includes("h")) {
-        setDugCellsNum(dugCellsRef.current.push(`${row}-${col}`));
-      }
-
-      if (board[row][col].includes("*")) {
-        setLost(true);
-      }
-
-      if (board[row][col].includes(" ") && board[row][col].includes("h")) {
-        for (let r = -1; r <= 1; r++) {
-          for (let c = -1; c <= 1; c++) {
-            let checking_row = row + r;
-            let checking_col = col + c;
-            if (
-              // we don't need to check the cell we passed
-              !(checking_row == row && checking_col == col)
-            ) {
-              if (
-                // checking_row and checking_col are inside the board
-                checking_row >= 0 &&
-                checking_row <= board.length - 1 &&
-                checking_col >= 0 &&
-                checking_col <= board.length - 1
-              ) {
-                if (
-                  !dugCellsRef.current.find(
-                    (element) => element == `${checking_row}-${checking_col}`
-                  )
-                ) {
-                  dig(checking_row, checking_col, board);
-                }
-              }
-            }
-          }
-        }
-      }
-      setBoard(
-        produce((draft) => {
-          draft[row][col] = draft[row][col].replace("h", "v");
-        })
-      );
-    }, []);
-
-    const value = item[0];
-    const flag = item[1];
-    const open = item[2];
-
-    return (
-      <div
-        className={`w-5 md:w-10 focus-within:ring ring-yellow-300 rounded ${
-          open.includes("h") ? "bg-gray-300" : "bg-green-300"
-        }`}
-      >
-        <button
-          className="w-full h-full"
-          onClick={() => {
-            dig(row, col, board);
-          }}
-        >
-          {open == "v" ? value : flag != "_" ? flag : " "}
-        </button>
-      </div>
-    );
-  }
-);
 
 import { create_board } from "../lib/utils";
