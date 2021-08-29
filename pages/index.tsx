46b2002e6be87c7f07fd141f875e6f20f154d726
Igor Bedesqui <igorbedesqui@gmail.com>
2021-08-28 21:40:17 -0300

    feat: abstracted Board to a component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useState, useRef } from "react";
+import { useState, useRef } from "react";
 import { useAtom } from "jotai";
 
-import Cell from "../components/Cell";
+import Board from "../components/Board";
 import {
   wonAtom,
   lostAtom,
@@ -10,14 +10,15 @@ import {
   bombNumberAtom,
 } from "../lib/store";
 
-export default function Board() {
+export default function Home() {
   const [won] = useAtom(wonAtom);
   const [lost, setLost] = useAtom(lostAtom);
-  const [dugCellsNum, setDugCellsNum] = useAtom(dugCellsNumAtom);
+  const [, setDugCellsNum] = useAtom(dugCellsNumAtom);
   const [dimSize] = useAtom(dimSizeAtom);
   const [bombNumber] = useAtom(bombNumberAtom);
 
   const dugCellsRef = useRef([]);
+  // this is passed to the cells so they can mutate an array of checked cells so we don't have to check cells more than once. This improves performance a lot.
 
   const [board, setBoard] = useState([]);
 
@@ -28,34 +29,10 @@ export default function Board() {
     setBoard(create_board(dimSize, bombNumber));
   };
 
-  useEffect(() => {
-    console.log(dugCellsRef.current.length, dugCellsNum);
-  }, [dugCellsNum]);
-
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="mb-6">Bombs: {bombNumber}</h1>
-      <main className="flex flex-col gap-1">
-        {board.map((rows, r) => {
-          return (
-            <div className="flex h-5 md:h-10 gap-1" key={`${r}`}>
-              {rows.map((item, c) => {
-                return (
-                  <Cell
-                    board={board}
-                    setBoard={setBoard}
-                    key={`${r}-${c}`}
-                    item={item}
-                    row={r}
-                    col={c}
-                    dugCellsRef={dugCellsRef}
-                  />
-                );
-              })}
-            </div>
-          );
-        })}
-      </main>
+      <Board board={board} setBoard={setBoard} dugCellsRef={dugCellsRef} />
       <h1>{lost ? "YOU LOST" : won ? "YOU WON" : "Not yet"}</h1>
       <button onClick={resetGame}>Reset game</button>
     </div>
