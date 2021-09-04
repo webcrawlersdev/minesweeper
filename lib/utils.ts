34d709a1ad7b593e1f029eb69685f9d92c9d5f5c
Igor Bedesqui <igorbedesqui@gmail.com>
2021-09-04 19:30:47 -0300

    feat: added methods for board with just numbers

--- lib/utils.ts
+++ lib/utils.ts
@@ -29,6 +29,72 @@ export const create_board = (dim_size, bomb_number) => {
   return board;
 };
 
+export const createBoardWithJustNumbers = (dim_size, bomb_number) => {
+  // create an array with the amount of cells the board will have
+  let board_array = new Array(dim_size * dim_size).fill(0);
+
+  // put the bombs in the board
+  board_array.fill(9, 0, bomb_number);
+
+  // suffle the board to distribute the bombs
+  board_array = fisherYatesShuffle(board_array);
+
+  // create a two dimentional array `board` from the suffled board
+  let board = singleToMultiDimentionalArray(board_array, dim_size);
+
+  // evaluate and assign the number of neighboring bombs for each cell
+  for (let r = 0; r <= dim_size - 1; r++) {
+    for (let c = 0; c <= dim_size - 1; c++) {
+      if (
+        // don't need to assign neighboringbombs to bomb cells
+        !(board[r][c] === 9)
+      ) {
+        board[r][c] = board[r][c] = getNumOfNeighboringBombsIfBombsAre9(
+          board,
+          r,
+          c
+        );
+      }
+    }
+  }
+
+  console.log(board);
+  return board;
+};
+
+export const getNumOfNeighboringBombsIfBombsAre9 = (
+  board: number[],
+  row,
+  col
+) => {
+  let bombNum = 0;
+
+  for (let r = -1; r <= 1; r++) {
+    for (let c = -1; c <= 1; c++) {
+      let checking_row = row + r;
+      let checking_col = col + c;
+      if (
+        // we don't need to check the cell we passed
+        !(checking_row == row && checking_col == col)
+      ) {
+        if (
+          // checking_row and checking_col are inside the board
+          checking_row >= 0 &&
+          checking_row <= board.length - 1 &&
+          checking_col >= 0 &&
+          checking_col <= board.length - 1
+        ) {
+          if (board[checking_row][checking_col] === 9) {
+            bombNum = bombNum + 1;
+          }
+        }
+      }
+    }
+  }
+
+  return bombNum;
+};
+
 /**  Credit:
  * http://bost.ocks.org/mike/shuffle/
  */
