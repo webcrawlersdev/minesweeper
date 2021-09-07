65e3aad098cf609710619eac5f76f818aebb83dc
Igor Bedesqui <igorbedesqui@gmail.com>
2021-09-07 17:32:23 -0300

    feat: applied overflow hidden on body

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import "../styles/global.css";
 
 function MyApp({ Component, pageProps }) {
+  globalStyles();
   return <Component {...pageProps} />;
 }
 
 export default MyApp;
+
+const globalStyles = globalCss({
+  body: { overflow: "hidden" },
+});
+
+import { globalCss } from "@stitches/react";
