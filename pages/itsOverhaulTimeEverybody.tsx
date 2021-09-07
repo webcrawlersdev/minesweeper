4c9aead672659e0d6d1cd1c92ca4b8e80259b211
Igor Bedesqui <igorbedesqui@gmail.com>
2021-09-07 19:00:40 -0300

    feat: added drag and pinch support

--- pages/itsOverhaulTimeEverybody.tsx
+++ pages/itsOverhaulTimeEverybody.tsx
@@ -1,6 +1,10 @@
 // sorry for the name future Igor, also, if someone else is seeing this, shoot me a tweet at @bedesqui
 
-// Genereate the board here!
+// https://v10-beta--use-gesture.netlify.app/docs/gestures/#about-the-pinch-gesture
+if (typeof document != "undefined") {
+  document.addEventListener("gesturestart", (e) => e.preventDefault());
+  document.addEventListener("gesturechange", (e) => e.preventDefault());
+}
 
 export default function Home() {
   const [board, setBoard] = useState<null | number[][]>(null);
@@ -16,10 +20,67 @@ export default function Home() {
     console.log(board);
   }, [board]);
 
+  //animation/gesture stuff bellow
+  const target = useRef(null);
+
+  const [style, api] = useSpring(() => ({
+    x: 0,
+    y: 0,
+    scale: 1,
+  }));
+
+  useGesture(
+    {
+      onDrag: ({ pinching, cancel, offset: [x, y], ...rest }) => {
+        if (pinching) return cancel();
+        api.start({ x, y });
+      },
+      onPinch: ({ origin: [ox, oy], first, offset: [s], memo }) => {
+        if (first) {
+          const { width, height, x, y } =
+            target.current.getBoundingClientRect();
+          const tx = ox - (x + width / 2);
+          const ty = oy - (y + height / 2);
+          memo = [style.x.get(), style.y.get(), tx, ty];
+        }
+        api.start({ scale: s });
+        return memo;
+      },
+      onWheel: ({ first, offset: [, s], memo }) => {
+        if (first) {
+          const { width, height, x, y } =
+            target.current.getBoundingClientRect();
+          const tx = x + width / 2;
+          const ty = y + height / 2;
+          memo = [style.x.get(), style.y.get(), tx, ty];
+        }
+        api.start({ scale: s / 800 });
+        return memo;
+      },
+    },
+    {
+      target: target,
+      eventOptions: { passive: false },
+      drag: { from: () => [style.x.get(), style.y.get()] },
+      pinch: { scaleBounds: { min: 0.2, max: 2 }, rubberband: true },
+      wheel: { scaleBounds: { min: 0.2, max: 2 }, rubberband: true },
+    }
+  );
+
   return (
-    <Box className={darkTheme}>
+    <Box className={darkTheme} ref={target} css={{ touchAction: "none" }}>
       {board && (
-        <GameHandler board={board} bombNumber={bombNumber} dimSize={dimSize} />
+        <animated.div
+          style={{
+            ...style,
+          }}
+        >
+          <GameHandler
+            board={board}
+            bombNumber={bombNumber}
+            dimSize={dimSize}
+          />
+        </animated.div>
       )}
     </Box>
   );
@@ -27,17 +88,22 @@ export default function Home() {
 
 const Box = styled("div", {
   minHeight: "100vh",
+  position: "relative",
 
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
+
+  overflow: "hidden",
   padding: "4rem",
 
   backgroundColor: "$background",
 });
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import GameHandler from "../components/GameHandler";
 import { createBoardWithJustNumbers } from "../lib/utils";
+import { useSpring, animated } from "@react-spring/web";
+import { useGesture } from "@use-gesture/react";
 import { styled } from "@stitches/react";
 import { darkTheme } from "../stitches.config";
