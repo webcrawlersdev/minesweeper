ab1253bdbb2a54e39d2891b108ed70dc3c06ec2f
Igor Bedesqui <igorbedesqui@gmail.com>
2021-09-09 20:02:02 -0300

    feat: added button reset

--- pages/_app.js
+++ pages/_app.js
@@ -15,6 +15,29 @@ export default MyApp;
 
 const globalStyles = globalCss({
   body: { overflow: "hidden" },
+
+  // Normalizing button
+  button: {
+    cursor: "pointer",
+
+    margin: 0,
+    padding: 0,
+
+    fontFamily: "inherit",
+    lineHeight: "inherit",
+    fontSize: "100%",
+    textTransform: "none",
+    color: "inherit",
+
+    backgroundColor: "transparent",
+    backgroundImage: "none",
+    border: "none",
+
+    "&:focus": {
+      outline: "1px dotted",
+      outline: "5px auto -webkit-focus-ring-color",
+    },
+  },
 });
 
 import Head from "next/head";
