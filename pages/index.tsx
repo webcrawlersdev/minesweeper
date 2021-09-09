1caf3ebfc9ae8b5b727c6a8e34dd35f970d6f4c6
Igor Bedesqui <igorbedesqui@gmail.com>
2021-09-09 19:38:16 -0300

    refactor: removed unused deps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,9 @@
-import { useState, useRef } from "react";
-import { useAtom } from "jotai";
-
-import Board from "../components/Board";
-import {
-  wonAtom,
-  lostAtom,
-  dugCellsNumAtom,
-  dimSizeAtom,
-  bombNumberAtom,
-} from "../lib/store";
-
 export default function Home() {
-  const [won] = useAtom(wonAtom);
-  const [lost, setLost] = useAtom(lostAtom);
-  const [, setDugCellsNum] = useAtom(dugCellsNumAtom);
-  const [dimSize] = useAtom(dimSizeAtom);
-  const [bombNumber] = useAtom(bombNumberAtom);
-
-  const dugCellsRef = useRef([]);
-  // this is passed to the cells so they can mutate an array of checked cells so we don't have to check cells more than once. This improves performance a lot.
-
-  const [board, setBoard] = useState([]);
-
-  const resetGame = () => {
-    dugCellsRef.current = [];
-    setDugCellsNum(0);
-    setLost(false);
-    setBoard(createBoardWithJustNumbers(dimSize, bombNumber));
-  };
-
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="mb-6">Bombs: {bombNumber}</h1>
-      <Board board={board} setBoard={setBoard} dugCellsRef={dugCellsRef} />
-      <h1>{lost ? "YOU LOST" : won ? "YOU WON" : "Not yet"}</h1>
-      <button onClick={resetGame}>Reset game</button>
-    </div>
+    <Link href="/itsOverhaulTimeEverybody" passHref>
+      <a>The game is here</a>
+    </Link>
   );
 }
 
-import { create_board, createBoardWithJustNumbers } from "../lib/utils";
+import Link from "next/link";
