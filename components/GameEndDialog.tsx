fde0191a09858358696d34b6e941a4830af8766c
Igor Bedesqui <igorbedesqui@gmail.com>
2021-09-16 21:16:00 -0300

    feat: added game end dialog

---
+++ components/GameEndDialog.tsx
@@ -0,0 +1,40 @@
+export const GameEndDialog = ({ playerWon, onClose }) => (
+  <Dialog className={dialog()} onClose={onClose}>
+    <h2 className={heading()}>
+      {playerWon ? "Congratulations!" : "Game over!"}
+    </h2>
+
+    <Box
+      css={{
+        display: "flex",
+        justifyContent: "space-evenly",
+      }}
+    >
+      <Button
+        outlined
+        onClick={() => {
+          console.log("hej");
+        }}
+      >
+        Try again
+        <ArrowRightIcon />
+      </Button>
+      <Button onClick={onClose}>close</Button>
+    </Box>
+  </Dialog>
+);
+
+const Box = styled("div");
+
+const dialog = css({
+  minWidth: 300,
+});
+
+const heading = css({
+  margin: "0 0 20px",
+});
+
+import { ArrowRightIcon } from "@radix-ui/react-icons";
+import Button from "./Button";
+import { Dialog } from "./Dialog";
+import { styled, css } from "../stitches.config";
