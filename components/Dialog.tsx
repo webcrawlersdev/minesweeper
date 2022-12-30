6841cf6880c098130f495f6444f6dca9b888e616
Igor Bedesqui <igorbedesqui@gmail.com>
2022-12-30 19:06:35 +0000

    feat: add blur, fix overlay appearing bellow board

--- components/Dialog.tsx
+++ components/Dialog.tsx
@@ -2,7 +2,7 @@ const Dialog = ({ children, ...props }) => {
   return (
     <DialogPrimitive.Root {...props}>
       <StyledOverlay />
-      {children}
+      <div style={{ zIndex: 50 }}>{children}</div>
     </DialogPrimitive.Root>
   );
 };
@@ -11,11 +11,15 @@ export default Dialog;
 
 const overlayShow = keyframes({
   "0%": { opacity: 0 },
-  "100%": { opacity: 1 },
+  "100%": { opacity: 0.1 },
 });
 
 const StyledOverlay = styled(DialogPrimitive.Overlay, {
-  backgroundColor: "$mauveA9",
+  backgroundColor: "$mauveA1",
+  backdropFilter: "blur(2px)",
+  zIndex: "50",
+
+  pointerEvents: "none",
   position: "fixed",
   inset: 0,
   "@motion": {
@@ -25,6 +29,7 @@ const StyledOverlay = styled(DialogPrimitive.Overlay, {
   variants: {
     open: {
       true: {
+        pointerEvents: "auto",
         "@motion": {
           animation: `${overlayShow} reverse 110ms cubic-bezier(0.4, 0.14, 1, 1)`,
         },
