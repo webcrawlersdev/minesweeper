5c682d5bd2e1fd467f85544b035f72f3617b3d99
Igor Bedesqui <igorbedesqui@gmail.com>
2023-01-01 18:02:54 +0000

    fix: floating timer container preventing clicks

--- components/FloatingTimer.tsx
+++ components/FloatingTimer.tsx
@@ -3,35 +3,28 @@ const FloatingTimer = () => {
 
   return (
     <Box
+      style={{ visibility: isVisible ? "visible" : "hidden" }}
       css={{
         position: "fixed",
         top: "1rem",
         zIndex: "10",
-        width: "100%",
 
+        margin: "0 auto",
         display: "flex",
         justifyContent: "center",
-        visibility: isVisible ? "visible" : "hidden",
+        alignItems: "center",
+        gap: "0.5rem",
+
+        backgroundColor: "$mauve3",
+        padding: "0.5rem",
+        borderRadius: "3px",
+        borderWidth: "1px",
+        borderStyle: "solid",
+        borderColor: "$mauve6",
       }}
     >
-      <Box
-        css={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          gap: "0.5rem",
-
-          backgroundColor: "$mauve3",
-          padding: "0.5rem",
-          borderRadius: "3px",
-          borderWidth: "1px",
-          borderStyle: "solid",
-          borderColor: "$mauve6",
-        }}
-      >
-        <StopwatchIcon />
-        <Timer />
-      </Box>
+      <StopwatchIcon />
+      <Timer />
     </Box>
   );
 };
